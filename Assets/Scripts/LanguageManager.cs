954260aa5206526e310af6bc97ad6281df8ce86c
LB <luisbazan417@gmail.com>
2018-05-26 19:11:38 -0500

    Language Manager

---
+++ Assets/Scripts/LanguageManager.cs
@@ -0,0 +1,22 @@
+﻿//Made by Luis Bazan
+//luisbernardo@golstats.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageManager : MonoBehaviour {
+
+    public Dropdown DRP_Language;
+
+    void Start () 
+	{
+        Debug.Log(DRP_Language.options[DRP_Language.value].text);
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
