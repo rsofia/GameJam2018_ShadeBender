74225866757b24ebe98d5b2f26d020597091bad1
Sofia Brown <ruthsofiab@gmail.com>
2018-05-26 19:35:10 -0500

    Creditos

---
+++ Assets/rsofia/Scripts/SCR_Scenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_Scenes : MonoBehaviour {
+
+	public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
