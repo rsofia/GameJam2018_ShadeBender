83ba7fcb1d1a8e29531ada7f73ec67b90552974f
LB <luisbazan417@gmail.com>
2018-05-27 03:20:44 -0500

    Enemies Fixed

---
+++ Assets/SetEnemyColor.cs
@@ -0,0 +1,22 @@
+﻿//Made by Luis Bazan
+//luisbernardo@golstats.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetEnemyColor : MonoBehaviour {
+
+    void OnEnable()
+    {
+        if (gameObject.layer == 8)
+        {
+            GetComponentInChildren<SkinnedMeshRenderer>().material.SetColor("_Color", SCR_CustomizationColor.colorAngel);
+        }
+
+        if (gameObject.layer == 9)
+        {
+            GetComponentInChildren<SkinnedMeshRenderer>().material.SetColor("_Color", SCR_CustomizationColor.colorDemon);
+        }
+    }
+}
